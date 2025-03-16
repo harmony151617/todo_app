29a5549e2c65aeda5f0548acaf3bd8705327c077
Helena <harmony151617@gmail.com>
2025-03-16 22:59:18 +0200

    added AuthPage

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const PORT = process.env.PORT || 5000
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://admin:admin@cluster0.foua9.mongodb.net/todo?retryWrites=true&w=majority&appName=Cluster0', {
-            useNewUrlParser: true,
+        await mongoose.connect('mongodb+srv://admin:admin@cluster0.foua9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+            /*useNewUrlParser: true,
             useUnifiedTopology: true,
-            /*useCreateIndex: true,
+            useCreateIndex: true,
             useFindAndModify: true*/
         })
          app.listen(PORT, () => {
-            console.log('Server started on port: ${}')
+            console.log(`Server started on port: ${PORT} `);
          })
     } catch (err) {console.error(err)}
     
