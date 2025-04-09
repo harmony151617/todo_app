74428699e95bf7ac26a68af28f653dfa66d26b42
Helena <harmony151617@gmail.com>
2025-04-09 23:38:19 +0300

    added auth.route

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(express.json({extended:true}))
+app.use('/api/auth', require('./routes/auth.route'))
+
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://admin:admin@cluster0.foua9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        await mongoose.connect('mongodb://admin:admin@cluster0-shard-00-00.foua9.mongodb.net:27017,cluster0-shard-00-01.foua9.mongodb.net:27017,cluster0-shard-00-02.foua9.mongodb.net:27017/?replicaSet=atlas-agzbnv-shard-0&ssl=true&authSource=admin', {
             /*useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
@@ -15,7 +18,7 @@ async function start() {
          app.listen(PORT, () => {
             console.log(`Server started on port: ${PORT} `);
          })
-    } catch (err) {console.error(err)}
+    } catch (error) {console.log(error)}
     
 }
 start()
